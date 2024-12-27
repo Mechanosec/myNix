1a21310aac79eef5cf193abb961621e52610b585
O.Krasovskyi <mechanosec29@gmail.com>
2024-12-27 23:56:52 +0200

    new mind (2024-12-27 23:56:51)

--- save_system.sh
+++ save_system.sh
@@ -3,7 +3,10 @@
 current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
 cp -r /etc/nixos/ ~/.config/home-manager/
+
 git pull
 git add .
 git commit -m "new mind ($current_datetime)"
 git push
+
+rm -rf ~/.config/home-manager/nixos
